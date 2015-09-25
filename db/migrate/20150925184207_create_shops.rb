c203a3c6f9fb1f87585d6099acdaadc70491d996
Julien Poncelet <julienponcelet7@gmail.com>
2015-09-25 21:13:50 +0200

    fix

--- db/migrate/20150925184207_create_shops.rb
+++ db/migrate/20150925184207_create_shops.rb
@@ -1,6 +1,10 @@
 class CreateShops < ActiveRecord::Migration
   def change
     create_table :shops do |t|
+      t.string    :title
+      t.string    :phone
+      t.string    :address
+      t.string    :job
 
       t.timestamps null: false
     end
