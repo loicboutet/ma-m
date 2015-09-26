f157f170ed433267a546b8c3e72eb2b8018b6271
Loïc Boutet <loic@boutet.com>
2015-09-26 20:19:11 +0200

    integrate shop search

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -41,6 +41,21 @@ class Shop < ActiveRecord::Base
     self.ratings.where(like: true).count
   end
 
+
+  def stars
+    stars = ['empty', 'empty', 'empty', 'empty', 'empty']
+    stars[0] = 'half' if self.average_stars >= 0.5
+    stars[0] = 'full' if self.average_stars >= 1
+    stars[1] = 'half' if self.average_stars >= 1.5
+    stars[1] = 'full' if self.average_stars >= 2
+    stars[2] = 'half' if self.average_stars >= 2.5
+    stars[2] = 'full' if self.average_stars >= 3
+    stars[3] = 'half' if self.average_stars >= 3.5
+    stars[3] = 'full' if self.average_stars >= 4
+    stars[4] = 'half' if self.average_stars >= 4.5
+    stars[4] = 'full' if self.average_stars >= 5
+    return stars
+  end
   # -- Methods
 
   # Uploader
