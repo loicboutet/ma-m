dd3c742158febd66d5fac452eb7fc2a3841df03e
Julien Poncelet <julienponcelet7@gmail.com>
2015-09-26 02:17:01 +0200

    Ratings

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,23 @@
 class Shop < ActiveRecord::Base
+  # Associations
+
+  has_many :ratings
+
+  # -- Associations
+
+  # Methods
+
+  def average_stars
+    self.ratings.pluck(:stars).sum / self.ratings.count
+  end
+
+  def dislikes
+    self.ratings.where(like: false)
+  end
+
+  def likes
+    self.ratings.where(like: true)
+  end
+
+  # -- Methods
 end
