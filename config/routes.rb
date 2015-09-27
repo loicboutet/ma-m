5abe9bd26be9f64fa68a290c7380a31275e0ee9e
Julien Poncelet <julienponcelet7@gmail.com>
2015-09-27 12:33:15 +0200

    Root to maps

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :comments
   resources :ratings
-  root to: 'visitors#index'
+  root to: 'maps#index'
   devise_for :users
   resources :users
   resources :shops
